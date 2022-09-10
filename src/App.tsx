4d381cef07b57b7cf07e5db22bbd50a20b2bf5ba
Paul Racisz <paulieracisz@gmail.com>
2022-09-10 14:08:20 -0400

    added some basic text and a background

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <main lang="en" className="App">
       <header className="App-header">
-      <h1 className="main-header">Hello</h1>
+        <h1 className="main-header">Next Level Performance</h1>
       </header>
+      <div className="black-text-box">
+        <h2>For Half The Cost</h2>
+        <p className="flavor-text">
+          Next Level Performance Supplements has the best dietary supplements,
+          designed to keep you at the top of your game.
+        </p>
+      </div>
       <footer></footer>
     </main>
   );
